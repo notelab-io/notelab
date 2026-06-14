36e870d4725b502557611854b4d62801acc91ecc
Sreeragh S <hey@sreeragh.me>
2026-06-14 09:59:53 +0530

    Add formula setup UI

---
+++ apps/web/src/editor/extensions/database/formula/index.ts
@@ -0,0 +1,12 @@
+export { DatabaseFormulaDialog } from "./database-formula-dialog"
+export { DatabaseFormulaValue } from "./database-formula-value"
+export {
+  escapeFormulaString,
+  getFormulaExpression,
+  getMergedFormulaConfig,
+} from "./formula-config"
+export {
+  evaluateDatabaseFormula,
+  formatFormulaValue,
+  getFormulaValueType,
+} from "./formula-engine"
